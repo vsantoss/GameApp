ad3e86708e8e0e1d5e67bd741fb5a24a53066d9c
vsantoss <santosvictor1997@gmail.com>
2023-11-08 20:28:14 +0100

    Add news

---
+++ app/src/main/java/com/vsanto/gameapp/ui/news/NewsFragment.kt
@@ -0,0 +1,21 @@
+package com.vsanto.gameapp.ui.news
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vsanto.gameapp.databinding.FragmentNewsBinding
+
+class NewsFragment : Fragment() {
+
+    private var _binding: FragmentNewsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
